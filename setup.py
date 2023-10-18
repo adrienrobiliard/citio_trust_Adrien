282a39176a900b38299c4502ec02868a5e9d3cb2
Adrien Robiliard <adrien@MacBook-Pro-de-Adrien.local>
2023-10-18 15:59:03 +0200

    new files for the package creation

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from setuptools.command.install import install
+from citio_trust_in_adrien.display import display_infos
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        install.run(self)
+        display_infos()
+
+
+setup(
+    name="citio_trust_in_adrien",
+    packages=["citio_trust_in_adrien"],
+    version="0.1.0",
+    license="MIT",
+    description="Package created for a presentation about malwares.",
+    author="ADRIEN ROBILIARD",
+    author_email="adrien@cit.io",
+    url="https://github.com/adrienrobiliard/citio_trust_in_Adrien",
+    download_url="https://github.com/user/reponame/archive/v_01.tar.gz",
+    keywords=["CITIO", "DEMO", "MALWARE"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",  # Specify which pyhton versions that you want to support
+    ],
+    cmdclass={
+        "install": PostInstallCommand,
+    },
+)
